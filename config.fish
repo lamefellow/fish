4700f9e462514571f84d0e32f014d5aecaa0595f
lamefellow <artemholubiev@outlook.com>
2026-08-25 17:37:01 +0300

    Add files via upload

--- config.fish
+++ config.fish
@@ -2,9 +2,11 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 
-fish_add_path /home/toxid/.spicetify
+fish_add_path /home/lame/.spicetify
 
 function fish_greeting
 	ramsay
 	echo
 end
+
+alias rf="rm -rf"
